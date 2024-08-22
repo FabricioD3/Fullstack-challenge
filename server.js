07346db7be0c4239dd6db4e0d023e1b7bf5e5744
Fabricio <oliveirafabricio419@gmail.com>
2024-08-21 22:37:08 -0300

    Conectando ao Mongodb Atlas

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const participantRoutes = require('./routes/participantRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-mongoose.connect('mongodb://localhost:27017/participants', {
-});
+require('dotenv').config();
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,3 +16,12 @@ app.use('/api/participants', participantRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Conectado ao MongoDB Atlas'))
+.catch(err => console.error('Erro ao conectar ao MongoDB', err));
