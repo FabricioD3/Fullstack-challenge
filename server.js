06c49eed11c4f8172630bf9ad6dfb4f3773df056
Fabricio <oliveirafabricio419@gmail.com>
2024-08-21 22:44:55 -0300

    Atualizando o server.js

--- server.js
+++ server.js
@@ -20,8 +20,6 @@ app.listen(PORT, () => {
 
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
 .then(() => console.log('Conectado ao MongoDB Atlas'))
 .catch(err => console.error('Erro ao conectar ao MongoDB', err));
